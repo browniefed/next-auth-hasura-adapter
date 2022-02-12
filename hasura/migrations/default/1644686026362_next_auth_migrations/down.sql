a14efcdc6c8e280c1193e28b0062dfe23d346490
Jason Brown <browniefed@gmail.com>
2022-02-12 12:23:05 -0800

    Adds meta/migrations

---
+++ hasura/migrations/default/1644686026362_next_auth_migrations/down.sql
@@ -0,0 +1,4 @@
+DROP TABLE "public"."accounts";
+DROP TABLE "public"."sessions";
+DROP TABLE "public"."verification_tokens";
+DROP TABLE "public"."users";
--- hasura/migrations/default/1644686026362_next_auth_migrations/up.sql
+++ hasura/migrations/default/1644686026362_next_auth_migrations/up.sql
@@ -1,11 +1,5 @@
-# Next Auth Hasura Adapter
-
-## Migration
-
-```sql
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE TABLE "public"."users" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "name" text, "email" text, "emailVerified" timestamptz NOT NULL, "image" text, PRIMARY KEY ("id") , UNIQUE ("email"));
 CREATE TABLE "public"."sessions" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "sessionToken" text NOT NULL, "expires" timestamptz NOT NULL, "userId" uuid NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON UPDATE restrict ON DELETE restrict);
 CREATE TABLE "public"."verification_tokens" ("token" text NOT NULL, "expires" timestamptz NOT NULL, "identifier" text NOT NULL, PRIMARY KEY ("token") );
 CREATE TABLE "public"."accounts" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "type" text NOT NULL, "provider" text NOT NULL, "providerAccountId" text NOT NULL, "refresh_token" text, "access_token" text, "expires_at" bigint, "token_type" text, "scope" text, "id_token" text, "session_state" text, "oauth_token_secret" text, "oauth_token" text, "user_id" uuid NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON UPDATE restrict ON DELETE restrict);
-```
